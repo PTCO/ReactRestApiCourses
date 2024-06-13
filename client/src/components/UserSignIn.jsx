004339411f90aeb8767d15b85d56903227952c81
Brandon Durand <ptco.dev@gmail.com>
2024-06-13 17:08:36 -0500

    Update UserSignIn.jsx

--- client/src/components/UserSignIn.jsx
+++ client/src/components/UserSignIn.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useRef } from "react";
 import UserContext from "../context/userContext";
 import { useLocation } from "react-router-dom";
 
